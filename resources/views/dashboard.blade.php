127dbcd71a707e7033abe2e779c6c147d96ef9d4
dheepss123 <dheepss321@gmail.com>
2024-04-21 09:25:13 +0700

    fix nav in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,5 @@
 <x-app-layout>
-    @include('components.navbar')
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Home') }}
-        </h2>
-    </x-slot>
+    @include('layouts.navigation')
     @include('components.hero')
     @include('components.section')
     @include('components.footer')
